ac114750c567139eff049fb6b80d7549967a23f6
shubhasankar <ssbhaumikdmr@gmail.com>
2021-08-12 22:53:39 +0530

    Added: On Submit Populate the Object

--- App.js
+++ App.js
@@ -1,17 +1,72 @@
- const salary = document.querySelector('#salary');
- const salaryOutput = document.querySelector('.salary-output');
- salaryOutput.textContent = salary.value;
- salary.oninput = function() {
-     salaryOutput.textContent = salary.value;
- };
-
- const name = document.querySelector('#name');
- const nameError = document.querySelector('.name-error');
- name.addEventListener('input', function() {
-     let nameRegex = RegExp('^[A-Z][a-z]{2,}$');
-     if(nameRegex.test(name.value))
-         nameError.textContent = "";
-     else
-         nameError.textContent = "Name is incorrect";
- });
-
+function cancelHover() {
+    document.getElementById("cancel").className = "cancelButtonOnHover";
+  }
+  function cancelHoverOut() {
+    document.getElementById("cancel").className = "btn";
+  }
+  function submitHover() {
+    document.getElementById("submitButton").className = "submitButtonOnHover";
+  }
+  function submitHoverOut() {
+    document.getElementById("submitButton").className = "submitButton";
+  }
+  function resetHover() {
+    document.getElementById("resetButton").className = "resetButtonOnHover";
+  }
+  function resetHoverOut() {
+    document.getElementById("resetButton").className = "resetButton";
+  }
+  // Event listener to select salary
+  const salary = document.querySelector('#salary');
+  const output = document.querySelector('.salary-output');
+  output.textContent = salary.value;
+  salary.addEventListener('input', function () {
+    output.textContent = salary.value;
+  });
+  // Save the data into a object
+  var save = () => {
+    try {
+      createEmployeePayroll();
+    } catch (e) {
+      return;
+    }
+  };
+  // Create the employee data object by getting their respective values
+  var createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    try {
+      employeePayrollData.name = getInputValueById("#name");
+      employeePayrollData.profilePic = getSelectedValues("[name=profile]").pop();
+      employeePayrollData.gender = getSelectedValues("[name=gender]").pop();
+      employeePayrollData.department = getSelectedValues("[name=department]");
+      employeePayrollData.salary = getInputValueById("#salary");
+      employeePayrollData.note = getInputValueById("#notes");
+      let date =
+        getInputValueById("#day") +
+        " " +
+        getInputValueById("#month") +
+        " " +
+        getInputValueById("#year");
+      employeePayrollData.startDate = new Date(Date.parse(date));
+      alert(employeePayrollData.toString());
+    } catch (e) {
+      setTextValue(".text-error", e);
+      throw e;
+    }
+    return employeePayrollData;
+  };
+  var getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selectedItems = [];
+    allItems.forEach((item) => {
+      if (item.checked) {
+        selectedItems.push(item.value);
+      }
+    });
+    console.log(selectedItems);
+    return selectedItems;
+  };
+  var getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+  };
