83e1d29d80a90254d7d07b414f89b02284596772
shubhasankar <ssbhaumikdmr@gmail.com>
2021-08-14 22:01:15 +0530

    Added: Store Object on Local Storage

--- App.js
+++ App.js
@@ -68,3 +68,14 @@ const getInputValueById = (id) => {
         .value;
     return value;
 }
+
+function createAndUpdateStorage(employeePayrollData) {
+  let employeePayrollList = JSON.parse( localStorage.getItem ("EmployeePayrollList"));
+  if(employeePayrollList != undefined) {
+     employeePayrollList.push(employeePayrollData);
+  } else {
+     employeePayrollList = [employeePayrollData];
+  }
+  alert(employeePayrollList.toString());
+  localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+}
